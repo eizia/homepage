651f89654335150f85ba83436a4624e92616f985
kevin14 <kevin14@me.com>
2015-08-27 10:35:27 +0800

    提交liveload以及路由

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.redirect('/home');
 });
+
+router.get('/:pageName', function(req, res) {
+	var filePath = path.join(__dirname,'../public/pages/'+req.params.pageName+'/index.html');
+	var html = fs.readFile(filePath,function(err,data){
+		if (err) {
+			res.status(404).end();
+		}else{
+			res.set('Content-Type', 'text/html');
+			res.send(data);
+		}
+	})
+})
 
 module.exports = router;

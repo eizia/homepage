9674b862b5339ff1f5f20d7ccdbe71252523ca7a
kevin14 <kevin14@me.com>
2015-09-06 16:57:12 +0800

    完成路由逻辑

--- public/pages/home/index.js
+++ public/pages/home/index.js
@@ -1,18 +1,49 @@
 define([], function() {
 
-    function initRouter() {
-        var routes = {
-            '/products': productsHander
-        };
+    var routes = {
+        '/':homehandler,
+        '/product': productHomeHandler,
+        '/product/:productName': productDetailHandler,
+        '/case':caseHomeHandler,
+        '/case/:caseName':caseDetailHandler,
+        '/customer':customerHomeHandler,
+        '/customer/:customerName':customerDetailHandler,
+        '/aboutus':aboutusHandler
+    };
 
-        function productsHander(){
-        	alert('products!');
-        }
-
-        var router = Router(routes);
-        router.init();
+    function homehandler(){
+    	console.log('this is home!')
     }
 
-    initRouter();
+    function productHomeHandler() {
+        console.log('product!');
+    }
+
+    function productDetailHandler(productName) {
+        console.log('product name is:'+productName);
+    }
+
+    function caseHomeHandler() {
+        console.log('case!');
+    }
+
+    function caseDetailHandler(caseName) {
+        console.log('case name is:'+caseName);
+    }
+
+    function customerHomeHandler() {
+        console.log('customer!');
+    }
+
+    function customerDetailHandler(caseName) {
+        console.log('customer name is:'+caseName);
+    }
+
+    function aboutusHandler() {
+        console.log('aboutus!');
+    }
+
+    var router = Router(routes);
+    router.init();
 
 });

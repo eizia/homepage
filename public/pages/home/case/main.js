55e0644d7b93a36dbf48c9342ff1e9216f5d86ad
kevin14 <kevin14@me.com>
2015-09-14 18:57:44 +0800

    提交点击案例的示例交互

--- public/pages/home/case/main.js
+++ public/pages/home/case/main.js
@@ -2,6 +2,8 @@ define(['less!./case.less'], function() {
     return {
         caseHomeHandler: function() {
             $('.guide').html('案例');
+            $('.module').removeClass('show');
+            $('#case').addClass('show');
             $('.mainVideoBg').css({
                 'background-image': 'url(/resource/image/bg_a.jpg)'
             })

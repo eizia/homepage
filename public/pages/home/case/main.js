797b709d1ef3eb0f192274cc35024a8de924bbf4
kevin14 <kevin14@me.com>
2015-09-19 12:29:36 +0800

    新增边框和case页面

--- public/pages/home/case/main.js
+++ public/pages/home/case/main.js
@@ -1,5 +1,12 @@
 define(['less!./case.less'], function() {
     var video = document.querySelector('.mainVideo.enter');
+
+
+    function showCases(){
+        var $caseWrapper = $('#case');
+        //加载case页面的内容
+    }
+
     return {
         caseHomeHandler: function() {
             $('.guideBtn').html('案例').attr('href','/#case');
@@ -9,6 +16,8 @@ define(['less!./case.less'], function() {
             $('.mainVideo.landing').remove();
             //替换视频并且开始播放
             video.play();
+
+            showCases();
         },
         caseDetailHandler: function(caseName) {
             console.log('case name is:' + caseName);
